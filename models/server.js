95755ee54ff482cf372d66e91e3fdd2579549787
ElevationUser <124209826+ElevationUser@users.noreply.github.com>
2023-05-10 16:33:31 -0300

    Update server.js
    Se pone default para el puerto

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ let colors = require('colors');
 class Server{
     constructor(){
         this.app=express();
-        this.port=process.env.PORT;
+        this.port=process.env.PORT || 3000;
         this.usuariosPath='/api/usuarios';
 
         //MidleWares
